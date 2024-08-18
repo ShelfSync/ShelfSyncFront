ac7f29eb23e4580fc7196358c32fadd85c66000a
itu-itis22-efen22 <efen22@itu.edu.tr>
2024-08-19 00:03:50 +0300

    header changed

--- src/main-page.js
+++ src/main-page.js
@@ -103,7 +103,7 @@ const MainPage = () =>{
           <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
             <div class="container-fluid">
               <div class="navbar-wrapper">
-                <a class="navbar-brand" href="javascript:void(0)">Library</a>
+                <a class="navbar-brand" href="javascript:void(0)">Buy us a coffee</a>
     
               </div>
               <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
