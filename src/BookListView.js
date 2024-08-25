d2a6da02633aa1329a9a6ebaf949e6511ae8695a
itu-itis22-efen22 <efen22@itu.edu.tr>
2024-08-25 21:43:29 +0300

    list view added

---
+++ src/BookListView.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import BookDetails from './BookDetails';
+// import './BookStyles.css';
+
+const BookListView = ({ books }) => {
+  const [selectedBook, setSelectedBook] = useState(null);
+  const [visibleColumns, setVisibleColumns] = useState({
+    title: true,
+    author: true,
+    altAuthor: true,
+    publisher: true,
+    year: true,
+    page: true,
+    readDate: true,
+    addedDate: true,
+    version: true,
+    categories: true,
+    description: true
+  });
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchCategory, setSearchCategory] = useState('title');
+
+  const handleBookClick = (book) => {
+    setSelectedBook(book);
+  };
+
+  const closeDetails = () => {
+    setSelectedBook(null);
+  };
+
+  const toggleColumn = (column) => {
+    setVisibleColumns(prevState => ({
+      ...prevState,
+      [column]: !prevState[column]
+    }));
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setSearchCategory(event.target.value);
+  };
+
+  const filteredBooks = books.filter((book) => {
+    const term = searchTerm.toLowerCase();
+    if (searchCategory === 'title') {
+      return book.title.toLowerCase().includes(term);
+    } else if (searchCategory === 'year') {
+      return book.year.toLowerCase().includes(term);
+    } else if (searchCategory === 'category') {
+      return book.categories.some((category) => category.toLowerCase().includes(term));
+    } else if (searchCategory === 'publisher') {
+      return book.publisher.toLowerCase().includes(term);
+    } else if (searchCategory === 'author') {
+        return book.author.toLowerCase().includes(term);
+    }
+    return true;
+  });
+
+  return (
+    <div className="book-table-container">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <select className='search-options' value={searchCategory} onChange={handleCategoryChange}>
+          <option value="title">Title</option>
+          <option value="year">Year</option>
+          <option value="category">Category</option>
+            <option value="publisher">Publisher</option>
+            <option value="author">Author</option>
+        </select>
+      </div>
+
+      <div className="filter-buttons">
+        <button onClick={() => toggleColumn('title')}>Title</button>
+        <button onClick={() => toggleColumn('author')}>Author</button>
+        <button onClick={() => toggleColumn('altAuthor')}>Alt Author</button>
+        <button onClick={() => toggleColumn('publisher')}>Publisher</button>
+        <button onClick={() => toggleColumn('year')}>Year</button>
+        <button onClick={() => toggleColumn('page')}>Page Count</button>
+        <button onClick={() => toggleColumn('readDate')}>Read Date</button>
+        <button onClick={() => toggleColumn('addedDate')}>Added Date</button>
+        <button onClick={() => toggleColumn('version')}>Version</button>
+        <button onClick={() => toggleColumn('categories')}>Categories</button>
+        <button onClick={() => toggleColumn('description')}>Description</button>
+      </div>
+
+      <div className="book-table">
+        <div className="book-table-header" style={{ fontWeight: 'bold' }}>
+          {visibleColumns.title && <div className="book-table-column">Title</div>}
+          {visibleColumns.author && <div className="book-table-column">Author</div>}
+          {visibleColumns.altAuthor && <div className="book-table-column">Alt Author</div>}
+          {visibleColumns.publisher && <div className="book-table-column">Publisher</div>}
+          {visibleColumns.year && <div className="book-table-column">Year</div>}
+          {visibleColumns.page && <div className="book-table-column">Page Count</div>}
+          {visibleColumns.readDate && <div className="book-table-column">Read Date</div>}
+          {visibleColumns.addedDate && <div className="book-table-column">Added Date</div>}
+          {visibleColumns.version && <div className="book-table-column">Version</div>}
+          {visibleColumns.categories && <div className="book-table-column">Categories</div>}
+          {visibleColumns.description && <div className="book-table-column">Description</div>}
+        </div>
+
+        {filteredBooks.map((book) => (
+          <div key={book.id} className="book-table-row" onClick={() => handleBookClick(book)}>
+            {visibleColumns.title && <div className="book-table-column">{book.title}</div>}
+            {visibleColumns.author && <div className="book-table-column">{book.author}</div>}
+            {visibleColumns.altAuthor && <div className="book-table-column">{book.altAuthor}</div>}
+            {visibleColumns.publisher && <div className="book-table-column">{book.publisher}</div>}
+            {visibleColumns.year && <div className="book-table-column">{book.year}</div>}
+            {visibleColumns.page && <div className="book-table-column">{book.page}</div>}
+            {visibleColumns.readDate && <div className="book-table-column">{book.readDate}</div>}
+            {visibleColumns.addedDate && <div className="book-table-column">{book.addedDate}</div>}
+            {visibleColumns.version && <div className="book-table-column">{book.version}</div>}
+            {visibleColumns.categories && (
+              <div className="book-table-column categories-column">
+                {book.categories.map((category, index) => (
+                  <div key={index} className="book-category-item">{category}</div>
+                ))}
+              </div>
+            )}
+            {visibleColumns.description && <div className="book-table-column">{book.description}</div>}
+          </div>
+        ))}
+      </div>
+      {selectedBook && <BookDetails book={selectedBook} closeDetails={closeDetails} />}
+    </div>
+  );
+};
+
+export default BookListView;
--- src/main-page.js
+++ src/main-page.js
@@ -2,25 +2,175 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './material-dashboard.css';
 import BookList from './BookList';
+import BookListView from './BookListView';
 
 
 
 const MainPage = () =>{
     const [Books, setBooks] = useState(null);
     const [isActive, setIsActive] = useState(false);
+    const [listActive, setListActive] = useState(false);
     const [User, setUser] = useState(null);
     const [showBookView, setShowBookView] = useState(false);
-    const [listView, setListView] = useState(false);
+    const [showListView, setListView] = useState(false);
+
+    const books = [
+      {
+        id: 1,
+        title: "Aristoteles'in Etik Kitapları",
+        readDate: "2022-01-15",
+        addedDate: "2022-01-16",
+        cover: '1',
+        description: 'Aristoteles’in etik düşünceleri üzerine bir inceleme.',
+        author: "John Smith",
+        altAuthor: "Emily Johnson",
+        publisher: "Oxford University Press",
+        version: "2",
+        year: "2022",
+        page: "350",
+        categories: ["Felsefe", "Etik", "Klasik"]
+      },
+      {
+        id: 2,
+        title: "Bilim ve Teknoloji Tarihi",
+        readDate: "2023-05-12",
+        addedDate: "2023-05-14",
+        cover: '2',
+        description: 'Bilim ve teknolojinin tarihsel gelişimini anlatan bir çalışma.',
+        author: "Alice Williams",
+        altAuthor: "Robert Brown",
+        publisher: "Cambridge University Press",
+        version: "1",
+        year: "2023",
+        page: "420",
+        categories: ["Bilim", "Teknoloji", "Tarih"]
+      },
+      {
+        id: 3,
+        title: "Orta Çağ Felsefesi",
+        readDate: "2022-11-23",
+        addedDate: "2022-11-25",
+        cover: '3',
+        description: 'Orta Çağ felsefesinin önemli düşünürleri ve teorileri.',
+        author: "Sarah Davis",
+        altAuthor: "Michael Clark",
+        publisher: "Harvard University Press",
+        version: "3",
+        year: "2022",
+        page: "280",
+        categories: ["Felsefe", "Orta Çağ", "Teori"]
+      },
+      {
+        id: 4,
+        title: "Modern Sanatın Temelleri",
+        readDate: "2023-02-09",
+        addedDate: "2023-02-10",
+        cover: '4',
+        description: 'Modern sanat akımlarının gelişimi ve etkileri.',
+        author: "David Wilson",
+        altAuthor: "Linda Martinez",
+        publisher: "Princeton University Press",
+        version: "1",
+        year: "2023",
+        page: "350",
+        categories: ["Sanat", "Modern", "Tarih"]
+      },
+      {
+        id: 5,
+        title: "Dijital Çağ ve Toplum",
+        readDate: "2024-06-18",
+        addedDate: "2024-06-20",
+        cover: '5',
+        description: 'Dijital teknolojilerin toplum üzerindeki etkileri ve değişimleri.',
+        author: "Laura Taylor",
+        altAuthor: "James Anderson",
+        publisher: "MIT Press",
+        version: "2",
+        year: "2024",
+        page: "400",
+        categories: ["Sosyoloji", "Dijital Teknoloji", "Toplum"]
+      },
+      {
+        id: 6,
+        title: "Kültürel Çalışmalar ve Medya",
+        readDate: "2023-07-30",
+        addedDate: "2023-08-01",
+        cover: '6',
+        description: 'Kültürel çalışmaların medya üzerindeki etkileri ve yansımaları.',
+        author: "Emma Wilson",
+        altAuthor: "Daniel Lee",
+        publisher: "University of Chicago Press",
+        version: "1",
+        year: "2023",
+        page: "320",
+        categories: ["Medya", "Kültürel Çalışmalar", "Sosyoloji"]
+      },
+      {
+        id: 7,
+        title: "Tarihsel Psikoloji",
+        readDate: "2024-01-25",
+        addedDate: "2024-01-26",
+        cover: '7',
+        description: 'Psikolojinin tarihsel gelişimi ve önemli teorileri.',
+        author: "William Green",
+        altAuthor: "Jessica White",
+        publisher: "Stanford University Press",
+        version: "2",
+        year: "2024",
+        page: "330",
+        categories: ["Psikoloji", "Tarih", "Teori"]
+      },
+      {
+        id: 8,
+        title: "Ekonomi ve Siyaset",
+        readDate: "2024-03-10",
+        addedDate: "2024-03-12",
+        cover: '8',
+        description: 'Ekonomik teorilerin siyaset üzerindeki etkileri.',
+        author: "Sophia Brown",
+        altAuthor: "Liam Harris",
+        publisher: "Yale University Press",
+        version: "1",
+        year: "2024",
+        page: "360",
+        categories: ["Ekonomi", "Siyaset", "Teori"]
+      },
+      {
+        id: 9,
+        title: "Bilimsel Araştırma Yöntemleri",
+        readDate: "2023-12-05",
+        addedDate: "2023-12-06",
+        cover: '9',
+        description: 'Bilimsel araştırma yöntemlerinin kapsamlı bir incelemesi.',
+        author: "Olivia Martin",
+        altAuthor: "Noah Clark",
+        publisher: "University of California Press",
+        version: "3",
+        year: "2023",
+        page: "380",
+        categories: ["Bilim", "Araştırma", "Metodoloji"]
+      }
+    ];
+    
 
 
     const handleLibraryClick = () => {
       setIsActive(!isActive);  // Aktif durumu tersine çevirir
-      setShowBookView(true);
+      if(!isActive){
+        setListActive(false);
+        setShowBookView(true);
+      } else if (isActive){ setShowBookView(false); }
+      
     };
 
+    
+
     const handleListViewClick = () => {
-      setIsActive(!isActive);  // Aktif durumu tersine çevirir
-      setListView(true);
+      setShowBookView(false);
+      setListActive(!listActive);  // Aktif durumu tersine çevirir
+      if(!listActive){setListView(true);}
+      else if (listActive){setListView(false);}
+      
     };
 
     useEffect(() => {
@@ -104,6 +254,7 @@ const MainPage = () =>{
             <div class="container-fluid">
               <div class="navbar-wrapper">
                 <a class="navbar-brand" href="javascript:void(0)">Buy us a coffee</a>
+                <i class="material-icons">coffee</i>
     
               </div>
               <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
@@ -139,7 +290,8 @@ const MainPage = () =>{
                         </div>
                     </a>
                 </div> */}
-                {showBookView ? <BookList /> : ''}
+                {showBookView ? <BookList books = {books} /> : ''}
+                {showListView ? <BookListView books = {books} /> : ''}
             </div>
           </div>
           </div>
